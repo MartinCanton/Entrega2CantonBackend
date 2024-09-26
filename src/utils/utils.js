13299b7c47c0bf8c39861bd169260bb04c052745
Martin Canton <martin.canton96@hotmail.com>
2024-09-26 00:08:11 -0300

    Usermodel y modif utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,34 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import bcrypt from 'bcrypt';
+import passport from 'passport';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(dirname(__filename))
 
+export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+
+export const isValidPassword = (user, password) => bcrypt.compareSync(password, user.password)
+
+export const passportCall = (strategy, options = {}) => {
+    return async (req, res, next) => {
+        passport.authenticate(strategy, options, (err, user, info) => {
+            if (err) {
+                return next(err);
+            }
+            if (!user) {
+                if (info && info.name === 'TokenExpiredError') {
+                    res.clearCookie('jwt', { httpOnly: true, secure: flase });
+                    return res.status(401).json({ message: 'Token Expired' });
+                } else if (info && info.name === 'NoAuthToken') {
+                    return next({ status: 401, message: 'No auth token' });
+                }
+                return next();
+            }
+            req.user = user;
+            next();
+        })(req, res, next);
+    };
+};
 
 export default __dirname;
